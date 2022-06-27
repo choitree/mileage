b748849f5cbcb7f2bc8bd4e995fae3faf43b7cd0
Tree <gmldbs1109@naver.com>
2022-06-27 17:43:24 +0900

    repository 추가 [#5]
    - 각 entity별 repository 추가

---
+++ src/main/java/com/triple/mileage/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package com.triple.mileage.repository;
+
+import com.triple.mileage.domain.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Long, Event> {
+}
