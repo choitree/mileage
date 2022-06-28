8c21e48b03bae6ad7a254a43dfd961d17f7c28e8
Tree <gmldbs1109@naver.com>
2022-06-28 19:10:30 +0900

    리뷰 수정을 위해서 ReviewUpdateRequestDTO 추가

---
+++ src/main/java/com/triple/mileage/dto/ReviewUpdateRequestDTO.java
@@ -0,0 +1,26 @@
+package com.triple.mileage.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReviewUpdateRequestDTO {
+
+    private String type;
+    private String action;
+    private UUID reviewId;
+    private String content;
+
+    @JsonProperty("attachedPhotoIds")
+    private List<UUID> photoIds;
+
+}
