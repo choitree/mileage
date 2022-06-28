c1a1ca6581e668f913d9f1ad71ec02f6d184424e
Tree <gmldbs1109@naver.com>
2022-06-28 09:37:32 +0900

    샘플 데이터 생성
    - user, place, event에 대한 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO user(user_id, name)
+VALUES ('3ede0ef2-92b7-4817-a5f3-0c575361f745', 'test');
+
+
+INSERT INTO place(place_id, address, coordinates)
+VALUES ('2e4baf1c-5acb-4efb-a1af-eddada31b00f', '경기도 성남시 분당구 판교역로14번길 16', '37.3848526,127.1114427');
+
+INSERT INTO event(event_type)
+VALUES ('REVIEW');
