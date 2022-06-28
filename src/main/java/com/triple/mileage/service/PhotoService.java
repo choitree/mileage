b740ce7387c438dd29a07e8f956b695de28db083
Tree <gmldbs1109@naver.com>
2022-06-28 09:50:18 +0900

    리뷰에 관련된 사진을 별도로 저장하기 위해 서비스 분리

---
+++ src/main/java/com/triple/mileage/service/PhotoService.java
@@ -0,0 +1,30 @@
+package com.triple.mileage.service;
+
+import com.triple.mileage.domain.Photo;
+import com.triple.mileage.domain.Review;
+import com.triple.mileage.repository.PhotoRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PhotoService {
+
+    private final PhotoRepository photoRepository;
+
+    public PhotoService(PhotoRepository photoRepository) {
+        this.photoRepository = photoRepository;
+    }
+
+    @Transactional
+    public void convertAndSaveAllPhotos(List<String> fileNames, Review review) {
+        List<Photo> photos = new ArrayList<>();
+        for (String fileName : fileNames) {
+            Photo photo = new Photo(fileName, review);
+            photos.add(photo);
+        }
+        photoRepository.saveAll(photos);
+    }
+}
