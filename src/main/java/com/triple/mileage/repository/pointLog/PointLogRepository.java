58fec73529d91aaf7d89bf9681e91fa3b1eeceb5
Tree <gmldbs1109@naver.com>
2022-06-28 19:04:50 +0900

    querydsl 적용한 PointLog Repository 추가

--- src/main/java/com/triple/mileage/repository/pointLog/PointLogRepository.java
+++ src/main/java/com/triple/mileage/repository/pointLog/PointLogRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PointLogRepository extends JpaRepository<PointLog, Long> {
+public interface PointLogRepository extends JpaRepository<PointLog, Long>, PointLogRepositoryCustom {
 }
