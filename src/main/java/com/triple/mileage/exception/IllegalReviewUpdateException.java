54bc3c03096965f6731b7f36a4e7475ccc05d5e4
Tree <gmldbs1109@naver.com>
2022-06-28 18:56:09 +0900

    Exception 추가

---
+++ src/main/java/com/triple/mileage/exception/IllegalReviewUpdateException.java
@@ -0,0 +1,12 @@
+package com.triple.mileage.exception;
+
+public class IllegalReviewUpdateException extends RuntimeException {
+
+    public IllegalReviewUpdateException() {
+        super();
+    }
+
+    public IllegalReviewUpdateException(String message) {
+        super(message);
+    }
+}
