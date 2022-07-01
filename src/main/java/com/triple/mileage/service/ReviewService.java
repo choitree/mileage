ce3d078c16bed9e213ab33abc9d13a6f9c2972c6
Tree <gmldbs1109@naver.com>
2022-07-01 22:45:13 +0900

    EventService 삭제 [#13]

--- src/main/java/com/triple/mileage/service/ReviewService.java
+++ src/main/java/com/triple/mileage/service/ReviewService.java
@@ -10,7 +10,6 @@ import com.triple.mileage.repository.ReviewRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.UUID;
 
 @Service
 public class ReviewService {
@@ -18,17 +17,14 @@ public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final UserService userService;
     private final PlaceService placeService;
-    private final EventService eventService;
     private final PhotoService photoService;
     private final PointLogService pointLogService;
 
     public ReviewService(ReviewRepository reviewRepository, UserService userService,
-                         PlaceService placeService, EventService eventService,
-                         PhotoService photoService, PointLogService pointLogService) {
+                         PlaceService placeService, PhotoService photoService, PointLogService pointLogService) {
         this.reviewRepository = reviewRepository;
         this.userService = userService;
         this.placeService = placeService;
-        this.eventService = eventService;
         this.photoService = photoService;
         this.pointLogService = pointLogService;
     }
@@ -40,17 +36,16 @@ public class ReviewService {
         }
         User user = userService.findByUserId(reviewRequestDTO.getUserId());
         Place place = placeService.findByPlaceId(reviewRequestDTO.getPlaceId());
-        Event event = eventService.findByEventType(EventType.REVIEW.getType());
 
         int samePlaceReviewCount = reviewRepository.findAllByPlace(place).size();
-        Review review = Review.createReview(reviewRequestDTO, user, place, event);
+        Review review = Review.createReview(reviewRequestDTO, user, place);
         if (reviewRequestDTO.getPhotoIds().size() == 0 && reviewRequestDTO.getContent().isBlank()) {
             throw new IllegalReviewException("내용과 사진 모두 없는 상태로 리뷰를 작성할 수 없습니다.");
         }
-        reviewRepository.save(review);
 
         photoService.convertAndSaveAllPhotosByReview(reviewRequestDTO.getPhotoIds(), review);
         pointLogService.saveCreateReviewPointLog(user, review, reviewRequestDTO.getPhotoIds().size(), samePlaceReviewCount, "리뷰생성");
+        reviewRepository.save(review);
     }
 
     public void updateReview(ReviewUpdateRequestDTO reviewUpdateRequestDTO) {
@@ -70,7 +65,6 @@ public class ReviewService {
         review.updateReview(reviewUpdateRequestDTO);
 
         reviewRepository.save(review);
-
         photoService.deleteAllPhotoByReview(review);
         photoService.convertAndSaveAllPhotosByReview(reviewUpdateRequestDTO.getPhotoIds(), review);
         pointLogService.saveUpdateReviewPointLog(user, review, isOriginContentExist, originPhotoCount, reviewUpdateRequestDTO.getPhotoIds().size(), "리뷰수정");
@@ -85,7 +79,7 @@ public class ReviewService {
         User user = userService.findByUserId(review.getUser().getUserId());
 
         photoService.deleteAllPhotoByReview(review);
-        pointLogService.softDeletePointLog(review, user);
         reviewRepository.delete(review);
+        pointLogService.softDeletePointLog(review, user);
     }
 }
