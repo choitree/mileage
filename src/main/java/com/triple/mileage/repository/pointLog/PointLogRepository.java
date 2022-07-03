0ba72e8db65bb4cc4981d765b2e7714174b7c06d
Tree <gmldbs1109@naver.com>
2022-07-03 15:11:22 +0900

    포인트 로그 조회를 위한 메소드 생성

--- src/main/java/com/triple/mileage/repository/pointLog/PointLogRepository.java
+++ src/main/java/com/triple/mileage/repository/pointLog/PointLogRepository.java
@@ -1,9 +1,14 @@
 package com.triple.mileage.repository.pointLog;
 
 import com.triple.mileage.domain.PointLog;
+import com.triple.mileage.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PointLogRepository extends JpaRepository<PointLog, Long>, PointLogRepositoryCustom {
+
+        List<PointLog> findAllByUser(User user);
 }
