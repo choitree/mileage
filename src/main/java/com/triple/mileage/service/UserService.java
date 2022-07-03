720ff07e535d15506fb314c0424ff06e501697cc
Tree <gmldbs1109@naver.com>
2022-07-03 15:13:04 +0900

    유저의 포인트, 포인트 로그 조회를 위한 메소드 추가

--- src/main/java/com/triple/mileage/service/UserService.java
+++ src/main/java/com/triple/mileage/service/UserService.java
@@ -1,21 +1,36 @@
 package com.triple.mileage.service;
 
 import com.triple.mileage.domain.User;
+import com.triple.mileage.dto.PointLogResponseDTO;
+import com.triple.mileage.dto.UserPointLogResponseDTO;
+import com.triple.mileage.dto.UserPointResponseDTO;
 import com.triple.mileage.exception.UserNotFoundException;
 import com.triple.mileage.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final PointLogService pointLogService;
 
     public User findByUserId(String userId) {
         return userRepository.findByUserId(userId)
-                .orElseThrow(() -> new UserNotFoundException("리뷰를 등록하려는 유저가 존재하지 않습니다."));
+                .orElseThrow(() -> new UserNotFoundException("해당 유저가 존재하지 않습니다."));
+    }
+
+    public UserPointResponseDTO showUserPoint(String userId) {
+        User user = findByUserId(userId);
+        return UserPointResponseDTO.from(user);
+    }
+
+    public UserPointLogResponseDTO showUserPointLog(String userId) {
+        List<PointLogResponseDTO> pointLogResponseDTOS = pointLogService.showAllPointLogsByUser(userId);
+        User user = findByUserId(userId);
+        return UserPointLogResponseDTO.from(user, pointLogResponseDTOS);
     }
 }
